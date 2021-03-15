d9a3f433b7db7337290e17c3453a803e673d6450
YeongJu-Na <78033158+YeongJu-Na@users.noreply.github.com>
2021-03-15 17:37:16 +0900

    Add files via upload
    - dialog 생성하기 & 메뉴와 연결하기
    - 입력한 문자열을 대문자로 변환하여 출력하기, 문자열 길이 - CString이용

---
+++ CDLG_TEST1.cpp
@@ -0,0 +1,66 @@
+﻿// CDLG_TEST1.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "MFCApplication1.h"
+#include "CDLG_TEST1.h"
+#include "afxdialogex.h"
+
+
+// CDLG_TEST1 대화 상자
+
+IMPLEMENT_DYNAMIC(CDLG_TEST1, CDialogEx)
+
+CDLG_TEST1::CDLG_TEST1(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_CDLG_TEST1, pParent)
+{
+
+}
+
+CDLG_TEST1::~CDLG_TEST1()
+{
+}
+
+void CDLG_TEST1::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CDLG_TEST1, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON1, &CDLG_TEST1::OnBnClickedButton2)
+END_MESSAGE_MAP()
+
+
+// CDLG_TEST1 메시지 처리기
+
+
+BOOL CDLG_TEST1::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// TODO:  여기에 추가 초기화 작업을 추가합니다.
+	CFont cf;
+	cf.CreateFontW(36, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, L"Arial");
+	GetDlgItem(IDC_EDIT1)->SetWindowTextW(L"아녕하세여");
+	GetDlgItem(IDC_EDIT1)->SetFont(&cf);
+
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+				  // 예외: OCX 속성 페이지는 FALSE를 반환해야 합니다.
+}
+
+void CDLG_TEST1::OnBnClickedButton2()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+//	CEdit 
+	CString buf;
+	GetDlgItem(IDC_EDIT2)->GetWindowTextW(buf);			//edit2에 입력한 문자열 받아오기
+	GetDlgItem(IDC_EDIT3)->SetWindowTextW(buf.MakeUpper());		//buf가 upper로 바뀌는게 아니라 upper로 바꾼 복사본이 반환됨
+	
+	int n = buf.GetLength();
+	CString s;
+	s.Format(L"%d", n);
+	GetDlgItem(IDC_EDIT4)->SetWindowTextW(s);
+	
+}
---
+++ CDLG_TEST1.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+
+// CDLG_TEST1 대화 상자
+
+class CDLG_TEST1 : public CDialogEx
+{
+	DECLARE_DYNAMIC(CDLG_TEST1)
+
+public:
+	CDLG_TEST1(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CDLG_TEST1();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CDLG_TEST1 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+	afx_msg void OnBnClickedButton2();
+};
