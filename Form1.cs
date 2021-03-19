a0ba8093fb04b528ba2180480af608e744d68319
YeongJu-Na <78033158+YeongJu-Na@users.noreply.github.com>
2021-03-19 17:48:55 +0900

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -7,84 +7,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
-using myLibrary;
 
-namespace WindowsFormsEdit
+namespace WindowsFormsGraphic
 {
-    public partial class Form1 : Form       //메모장 기능 구현
+    public partial class Form1 : Form
     {
+        Graphics GDC;
+        //Pen pp=new Pen(Color.Red,3);    //초기값 
+
+        int width=2, px=10, py=10;
         public Form1()
         {
             InitializeComponent();
+            //GDC=CreateGraphics();   //form1다이얼로그 자체의 것(this)
+            GDC = Canvas.CreateGraphics();
         }
 
-        private void mnuFileOpen_Click(object sender, EventArgs e)
+        private void Canvas_Paint(object sender, PaintEventArgs e)
         {
-            DialogResult ret = openFileDialog1.ShowDialog();   //==DoModal() (C++)
+            //Pen pp = new Pen(Color.Red,10);
+            //e.Graphics.DrawEllipse(pp,100,100,200,200);
+        }
+
+        private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Pen pp = new Pen(Color.Red, width);
+                GDC.DrawEllipse(pp, e.X-px/2, e.Y-py/2, px, py);
+            }
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void Canvas_Resize(object sender, EventArgs e)
+        {
+            GDC = Canvas.CreateGraphics();
+
+        }
+
+        private void mnuEraseAll_Click(object sender, EventArgs e)
+        {
+            GDC.Clear(DefaultBackColor);
+        }
+       
+        private void mnuPenSet_Click(object sender, EventArgs e)
+        {
+            penSet dlg = new penSet(px,py,width);   //new penSet(cirX, cirY, thickness)-- 방법2.
+            DialogResult ret =  dlg.ShowDialog();
+
             if (ret == DialogResult.OK)
             {
-                string fName = openFileDialog1.FileName;    //full path, openFileDialog.SafeFileName;  // file name only
-                StreamReader sr = new StreamReader(fName);  //FILE* (C), CFile(c++)
-                tb1.Text = sr.ReadToEnd();
-                //Form1.ActiveForm.Text =fName;
-                sr.Close();
-                string[] arr = fName.Split('\\');      //char[], 백슬래시 이므로 한번 더 써줘야
-                //Form1.ActiveForm.Text = arr[arr.Length-1].Split('.')[0]; 
-                //this.Text= arr[arr.Length - 1].Split('.')[0];   //현재 폼==this로 접근 가능
-                int n = myLib.Count('\\', fName);
-                this.Text += myLib.GetToken(n, '\\', fName);
-                Font f = tb1.Font;
-                sbLabel1.Text = f.Name;
-                sbLabel2.Text = f.Size.ToString();
+                if (dlg.pwidth != -1) width = dlg.pwidth;
+                if (dlg.px != -1) px = dlg.px;
+                if (dlg.py != -1) py = dlg.py;
             }
-        }
-        private void font_display()
-        {
-            sbLabel1.Text = tb1.Font.Name;
-            sbLabel2.Text = $"{tb1.Font.Size}";
-        }
-        private void mnuFileSave_Click(object sender, EventArgs e)
-        {
-            DialogResult ret = saveFileDialog1.ShowDialog();        //호출, 생성
-            string fName = saveFileDialog1.FileName;
-            StreamWriter sw = new StreamWriter(fName);
-            if (ret == DialogResult.Cancel) return;
-            sw.Write(tb1.Text);
-            sw.Close();
-        }
 
-        private void mnuViewFont_Click(object sender, EventArgs e)
-        {
-            DialogResult ret = fontDialog1.ShowDialog();
-            if (ret == DialogResult.Cancel) return;
-
-            
-            Font f = fontDialog1.Font;
-            tb1.Font = f;
-            font_display();
-            /*
-            sbLabel1.Text = f.Name;
-            sbLabel2.Text = f.Size.ToString();
-            */
-        }
-
-        private void mnuFileNew_Click(object sender, EventArgs e)
-        {
-            tb1.Text = "";
-        }
-
-
-        private void lineToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            if (tb1.WordWrap) tb1.WordWrap = false;
-            else tb1.WordWrap = true;
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            font_display();
+            //py = int.Parse(ps.y);         //입력문자열이 잘못된 경우 바로 에러뜸
         }
     }
 }
---
+++ penSet.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsGraphic
+{
+    public partial class penSet : Form
+    {
+        public int pwidth;
+        public int px;
+        public int py;
+        public penSet(int x, int y, int t)
+        {
+            px = x; py= y; pwidth = t;
+            InitializeComponent();
+        }
+        
+        private void penSet_Load(object sender, EventArgs e)
+        {
+            tbThick.Text = $"{pwidth}";
+            tbX.Text = $"{px}";
+            tbY.Text = $"{py}";
+        }
+        private bool isNumericValue(string str)
+        {
+            char[] arr = str.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < '0' || arr[i] > '9') return false;
+            }
+            return true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+            if(tbThick.Text!="") thick = int.Parse(tbThick.Text);
+            if (tbX.Text != "") x = int.Parse(tbX.Text);
+            if (tbY.Text != "") y= int.Parse(tbY.Text);*/
+            string wid = tbThick.Text;
+            string xx = tbX.Text;
+            string yy = tbY.Text;
+
+            // 숫자가 아닌 값이 들어갔을 때 오류 발생 --> 예외 처리
+            if (!isNumericValue(wid) || !isNumericValue(xx) || !isNumericValue(yy))
+            {
+                MessageBox.Show("잘못된 문자열입니다.");
+                return;
+            }
+            //빈칸은 기존의 값 유지되도록 -1을 반환해 알림
+            if (wid != "") pwidth = int.Parse(wid);
+            else pwidth = -1;
+            if (xx != "") px = int.Parse(xx);
+            else px = -1;
+            if (yy != "") py = int.Parse(yy);
+            else py = -1;
+
+            
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
